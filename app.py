12ef4aa76d7a4ccb1322a9fcfa7ef9f6b5a8588e
vishnu <vishnu@vishnus-MacBook-Air.local>
2022-10-29 13:34:06 -0600

    v1.6

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from website.nlp_kng import config
 os.environ["CORENLP_HOME"] = config.CORENLP_DIR
 app = create_app()
 
-
 if __name__ == '__main__':
     print(f'calling main')
     app.run(debug=True, host='0.0.0.0')
